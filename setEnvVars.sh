d3cc0464d298d4e2150bad0493557c28b67b3029
Christopher Starks <cstarks@dminc.com>
2023-02-09 14:36:34 -0500

    Finished App

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="Sivi-i"
+export GITHUB_REPO="bootcamp-2022-sls-api-starks"
+export BUCKET_NAME="cstarks-bootcamp-2022-bucket" #username must be all lowercase
